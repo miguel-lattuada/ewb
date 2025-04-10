056df16bb6e6bd8231d5fa7327efb58a1c39005a
Miguel Lattuada <miguel.lattuada@policyme.com>
2025-04-09 23:31:26 -0300

    feat: configured tkinter

---
+++ python/browser.py
@@ -0,0 +1,20 @@
+''' Browser window '''
+import tkinter
+
+WIDTH = 800
+HEIGHT = 600
+
+class Browser:
+    '''
+    Browser class
+    '''
+    def __init__(self) -> None:
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
+        self.canvas.pack()
+
+    def load(self, url: str):
+        ''' Draw canvas '''
+        self.canvas.create_rectangle(10, 20, 400, 300)
+        self.canvas.create_oval(100, 100, 150, 150)
+        self.canvas.create_text(200, 150, text="Hi!")
---
+++ python/main.py
@@ -0,0 +1,34 @@
+import sys
+import tkinter
+import typing as t
+
+from browser import Browser
+import minify_html
+
+import ewb
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+
+    browser = Browser()
+    browser.load(url)
+
+    tkinter.mainloop()
+
+    # print(url)
+
+    # response = t.cast(str, ewb.request(url))
+
+    # # Remove line breaks
+    # response = minify_html.minify(
+    #     code=response,
+    #     minify_css=True,
+    #     minify_js=True,
+    #     keep_closing_tags=True,
+    #     preserve_brace_template_syntax=True,
+    #     keep_html_and_head_opening_tags=True,
+    # )
+
+    # node = ewb.load(response)
+
+    # print(node.data.tag_name)
